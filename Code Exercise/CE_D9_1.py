5d1f04391f476213357949e9aef73843d8939d90
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-04-12 20:27:49 +0530

    d9 code exercise

---
+++ Code Exercise/CE_D9_1.py
@@ -0,0 +1,12 @@
+# Write a program that asks users to enter a password. 
+# Then, it checks if the password length is greater than 7 
+# and returns "Great password there!".
+# If the password has 7 or fewer characters, the program 
+# returns, "Your password is weak".
+
+password = input('Enter a password: ')
+
+if len(password) > 7:
+	print('Great password there!')
+else:
+	print('Your password is weak')
