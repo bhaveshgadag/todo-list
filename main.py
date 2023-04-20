007007a9fac55445337b6445d446051e4c58ee50
Bhavesh Gadag <bhaveshgadag46@gmail.com>
2023-04-20 20:44:33 +0530

    D10::startswith & try-except (#8)
    * bug fix:replaced in with startswith
    
    * error handling for ediy and complete
    
    * code exercise

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ while True:
     user_action = input("Select an option, add, edit, show, complete or exit : ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:]
 
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + "\n")
 
         with open('files/todos.txt', 'w') as file:
             file.writelines(todos)
 
-    elif 'show' in user_action:
+    elif user_action.startswith("show"):
         with open('files/todos.txt', 'r') as file:
             todos = file.readlines()
 
@@ -23,33 +23,41 @@ while True:
             todo = f"{index + 1}. {item}"
             print(todo)
                 
-    elif 'edit' in user_action:
-        number = int(user_action[5:]) - 1
+    elif user_action.startswith("edit"):
+         try:
+         	number = int(user_action[5:]) - 1
+         	with open('files/todos.txt', 'r') as file:
+         		todos = file.readlines()
+         		existing_todo = todos[number]
+         		
+         		todos[number] = input("Editing '" + existing_todo.strip("\n") + "' : ") + "\n"
+         		
+         	with open('files/todos.txt', 'w') as file:
+         		file.writelines(todos)
+         except ValueError:
+         	print("Incorrect use of command")
+         	continue
 
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
+    elif user_action.startswith("complete"):
+         try:
+         	number = int(user_action[9:])
+         	index = number - 1
+         	
+         	with open('files/todos.txt', 'r') as file:
+         		todos = file.readlines()
+         	
+         	todo = todos[index].strip("\n")
+         	print(f"Removing {todo} from list as it is complete.")
+         	todos.pop(index)
+         	
+         	with open('files/todos.txt', 'w') as file:
+         		file.writelines(todos)
+         		
+         except IndexError:
+         	print("Value out of range.")
+         	continue
 
-        existing_todo = todos[number]
-
-        todos[number] = input("Editing '" + existing_todo.strip("\n") + "' : ") + "\n"
-        with open('files/todos.txt', 'w') as file:
-            file.writelines(todos)
-
-    elif 'complete' in user_action:
-        number = int(user_action[9:])
-        index = number - 1
-
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
-
-        todo = todos[index].strip("\n")
-        print(f"Removing {todo} from list as it is complete.")
-        todos.pop(index)
-
-        with open('files/todos.txt', 'w') as file:
-            file.writelines(todos)
-
-    elif 'exit' in user_action:
+    elif user_action.startswith("exit"):
         break
 
     else:
