233a77f0cab574346679cecbf520ed4aa6e9868b
Bhavesh Gadag <bhaveshgadag46@gmail.com>
2023-04-24 09:16:56 +0530

    D11::fn to get_todos (#9)
    * new fn get_todos
    
    * bonus exp & data file
    
    * code exercises

---
+++ Code Exercise/CE_D11_1.py
@@ -0,0 +1,18 @@
+# The code below is incomplete. It should calculate and 
+# print out the maximum value of the grades list.
+# Please complete the function and then call it.
+#
+# def get_max():
+#     grades = [9.6, 9.2, 9.7]
+# The output of your code should be:
+#
+# 9.7
+
+def get_max():
+    grades = [9.6, 9.2, 9.7]
+    max_local = max(grades)
+    return max_local
+    
+    
+max_value = get_max()
+print(max_value)
