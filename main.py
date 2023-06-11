52999771a95153f381b94cba1abb1e7ae77f2584
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-06-11 23:42:50 +0530

    refactor

--- main.py
+++ main.py
@@ -23,37 +23,37 @@ while True:
             item = item.strip('\n')
             todo = f"{index + 1}. {item}"
             print(todo)
-                
+
     elif user_action.startswith("edit"):
-         try:
-         	number = int(user_action[5:]) - 1
-         	todos = functions.get_todos()
-         	existing_todo = todos[number]
-         		
-         	todos[number] = input("Editing '" + existing_todo.strip("\n") + "' : ") + "\n"
-         		
-         	functions.write_todos(todos)
-         	
-         except ValueError:
-         	print("Incorrect use of command")
-         	continue
+        try:
+            number = int(user_action[5:]) - 1
+            todos = functions.get_todos()
+            existing_todo = todos[number]
+
+            todos[number] = input("Editing '" + existing_todo.strip("\n") + "' : ") + "\n"
+
+            functions.write_todos(todos)
+
+        except ValueError:
+            print("Incorrect use of command")
+            continue
 
     elif user_action.startswith("complete"):
-         try:
-         	number = int(user_action[9:])
-         	index = number - 1
-         	
-         	todos = functions.get_todos()
-         	
-         	todo = todos[index].strip("\n")
-         	print(f"Removing {todo} from list as it is complete.")
-         	todos.pop(index)
-         	
-         	functions.write_todos(todos)
-         		
-         except IndexError:
-         	print("Value out of range.")
-         	continue
+        try:
+            number = int(user_action[9:])
+            index = number - 1
+
+            todos = functions.get_todos()
+
+            todo = todos[index].strip("\n")
+            print(f"Removing {todo} from list as it is complete.")
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+        except IndexError:
+            print("Value out of range.")
+            continue
 
     elif user_action.startswith("exit"):
         break
