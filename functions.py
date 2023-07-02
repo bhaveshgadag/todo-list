b599908a6d93a7bc0edcad7fc28e5249a7849e89
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-07-02 23:58:07 +0530

    Implemented add button:refactor functions

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 FILEPATH = 'files/todos.txt'
 
-def get_todos(filepath=FILEPATH):
-	"""Function to read to-do list from file"""
-	with open(filepath, 'r') as file_local:
-		todos_local = file_local.readlines()
-	return todos_local
 
-		
-def write_todos(todos_local,filepath=FILEPATH):
-	"""Function to write to-do list to file"""
-	with open(filepath, 'w') as file_local:
-            file_local.writelines(todos_local)
+def get_todos(filepath=FILEPATH):
+    """Function to read to-do list from file"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_local, filepath=FILEPATH):
+    """Function to write to-do list to file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_local)
--- gui.py
+++ gui.py
@@ -1,10 +1,23 @@
 import functions
-import PySimpleGUI as psg
+import PySimpleGUI as sg
 
-label = psg.Text('Type in a todo:')
-user_input = psg.InputText(tooltip='Enter todo')
-button = psg.Button('Add')
+label = sg.Text('Type in a todo:')
+user_input = sg.InputText(tooltip='Enter todo', key='todo')
+add_button = sg.Button('Add')
 
-window = psg.Window('To-do list', layout=[[label],[user_input, button]])
+window = sg.Window('To-do list',
+                   layout=[[label],[user_input, add_button]],
+                   font=('Helvetica',20))
 
-window.read()
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todo = values['todo']
+            todos = functions.get_todos()
+            todos.append(todo + "\n")
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+window.close()
