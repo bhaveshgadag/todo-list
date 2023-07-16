eccad9fcaab3cd26ea25cfabc1408a02f316dc41
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-07-16 12:30:05 +0530

    Complete todo functionality for web app

--- web.py
+++ web.py
@@ -13,8 +13,14 @@ def add_todo():
 st.title("My Todo List")
 
 for index, todo in enumerate(todos):
-    st.checkbox(label=todo, key=index)
+    checkbox = st.checkbox(label=todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 st.text_input(label="Enter a new todo", placeholder="Add a new todo...",
-              on_change=add_todo, label_visibility=False,
+              on_change=add_todo, label_visibility='hidden',
               key="todo")
